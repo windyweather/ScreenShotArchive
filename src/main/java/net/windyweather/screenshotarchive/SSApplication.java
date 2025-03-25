7bd3bc2a751e2b44ff3e28607347093fc19f9503
Windy Player <windyplayer@gmx.com>
2025-03-24 18:36:35 -0700

    First commit of a working GUI

--- src/main/java/net/windyweather/screenshotarchive/SSApplication.java
+++ src/main/java/net/windyweather/screenshotarchive/SSApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SSApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("screen-shot-archive.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(SSApplication.class.getResource("screen-shot-archive.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
+        stage.setTitle("ScreenShotArchive");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/net/windyweather/screenshotarchive/SSController.java
+++ src/main/java/net/windyweather/screenshotarchive/SSController.java
@@ -1,14 +1,71 @@
 package net.windyweather.screenshotarchive;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollBar;
 
-public class HelloController {
+public class SSController {
+    public Button btnUpdatePair;
+    public Button btnRemovePair;
+    public Button btnAddPair;
+    public Button btnViewSource;
+    public Button btnViewDestination;
+    public Button btnCopySource;
+    public Button btnDeleteSource;
+    public Button btnCloseAppButton;
+    public MenuItem miSavePairs;
+    public MenuItem miCloseApplication;
+    public MenuItem miAboutApplication;
+    public Button btnGoImagesStart;
+    public ScrollBar sbSlideToImage;
+    public Button btnGoImagesEnd;
     @FXML
-    private Label welcomeText;
+    private Label lblImageName;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void lblImageName() {
+        lblImageName.setText("Welcome to JavaFX Application!");
+    }
+
+    public void OnMenuSavePairs(ActionEvent actionEvent) {
+    }
+
+    public void OnMenuCloseApplication(ActionEvent actionEvent) {
+    }
+
+    public void onAboutApplication(ActionEvent actionEvent) {
+    }
+
+    public void onGoImagesStart(ActionEvent actionEvent) {
+    }
+
+    public void onGoImagesEnd(ActionEvent actionEvent) {
+    }
+
+    public void btnAddPair(ActionEvent actionEvent) {
+    }
+
+    public void OnRemovePair(ActionEvent actionEvent) {
+    }
+
+    public void OnUpdatePair(ActionEvent actionEvent) {
+    }
+
+    public void OnViewSource(ActionEvent actionEvent) {
+    }
+
+    public void OnViewDestination(ActionEvent actionEvent) {
+    }
+
+    public void OnCopySource(ActionEvent actionEvent) {
+    }
+
+    public void OnDeleteSource(ActionEvent actionEvent) {
+    }
+
+    public void OnCloseAppButton(ActionEvent actionEvent) {
     }
 }
