ad3a622bc9e09bc501baeac0e6e847c12d985b73
Windy Player <windyplayer@gmx.com>
2025-04-11 10:58:49 -0700

    Add App wrapper class and confirm build of Jar with dependencies

---
+++ src/main/java/net/windyweather/screenshotarchive/JarMain.java
@@ -0,0 +1,10 @@
+package net.windyweather.screenshotarchive;
+
+import javafx.application.Application;
+
+public class JarMain {
+    public static void main(String[] args ) {
+        System.out.println("JarMain - app starting in main again");
+        Application.launch(SSApplication.class, args);
+    }
+}
